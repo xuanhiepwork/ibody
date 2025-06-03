f8abdf45e28f2e8a58257d49497acc713b5f74dc
Tran Xuan Hiep <xuanhiepwork@gmail.com>
2025-06-03 15:48:28 +0700

    [add] bổ sung database

--- database_structure.sql
+++ database_structure.sql
@@ -24,14 +24,65 @@ DROP TABLE IF EXISTS `account`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `account` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `usser_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
   `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `salt` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
   `hash` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
-  KEY `usser_id` (`usser_id`),
-  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`usser_id`) REFERENCES `user` (`id`)
+  KEY `usser_id` (`user_id`),
+  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `account_ibfk_2` FOREIGN KEY (`id`) REFERENCES `expert_profile` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointment` (
+  `appointment_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `expert_id` int NOT NULL,
+  `expert_available_slot_id` int NOT NULL,
+  `start_time` timestamp NOT NULL,
+  `end_time` timestamp NOT NULL,
+  `duration_minutes` int NOT NULL,
+  `service_description` text COLLATE utf8mb4_general_ci NOT NULL,
+  `status` enum('pending_confirmation','confirmed','cancelled','completed') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'pending_confirmation',
+  `cancellation_reason` text COLLATE utf8mb4_general_ci NOT NULL,
+  `related_call_id` int NOT NULL,
+  `invoice_item_id` int NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `appointment_id` (`appointment_id`),
+  KEY `user_id` (`user_id`),
+  KEY `expert_id` (`expert_id`),
+  CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`appointment_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `calls`
+--
+
+DROP TABLE IF EXISTS `calls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `calls` (
+  `call_id` int NOT NULL AUTO_INCREMENT,
+  `caller_id` int NOT NULL,
+  `recevier_id` int NOT NULL,
+  `duration_minutes` int DEFAULT NULL,
+  `start_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status call_status` int NOT NULL,
+  `cost` float NOT NULL,
+  PRIMARY KEY (`call_id`),
+  CONSTRAINT `calls_ibfk_1` FOREIGN KEY (`call_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -45,7 +96,129 @@ DROP TABLE IF EXISTS `chatbox`;
 CREATE TABLE `chatbox` (
   `id` int NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_message_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `conversations`
+--
+
+DROP TABLE IF EXISTS `conversations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `conversations` (
+  `conversation_id` int NOT NULL,
+  PRIMARY KEY (`conversation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `expert_available_slots`
+--
+
+DROP TABLE IF EXISTS `expert_available_slots`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `expert_available_slots` (
+  `slot_id` int NOT NULL AUTO_INCREMENT,
+  `expert_id` int NOT NULL,
+  `start_time` timestamp NOT NULL,
+  `end_time` timestamp NOT NULL,
+  `status` enum('avail','busy') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'avail',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `slot_id` (`slot_id`),
+  KEY `expert_id` (`expert_id`),
+  CONSTRAINT `expert_available_slots_ibfk_1` FOREIGN KEY (`slot_id`) REFERENCES `expert_profile` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `expert_payout_methods`
+--
+
+DROP TABLE IF EXISTS `expert_payout_methods`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `expert_payout_methods` (
+  `payout_method_id` int NOT NULL AUTO_INCREMENT,
+  `expert_id` int NOT NULL,
+  `account_details` tinyint(1) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `payout_method_id` (`payout_method_id`),
+  KEY `expert_id` (`expert_id`),
+  CONSTRAINT `expert_payout_methods_ibfk_1` FOREIGN KEY (`payout_method_id`) REFERENCES `payout_requests` (`payout_request_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `expert_profile`
+--
+
+DROP TABLE IF EXISTS `expert_profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `expert_profile` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `specialization` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci NOT NULL,
+  `hourly_rate` float NOT NULL,
+  `availability` json DEFAULT NULL,
+  `years_of_experience` int DEFAULT NULL,
+  `is_verified` tinyint(1) NOT NULL DEFAULT '0',
+  `average_rating` float NOT NULL DEFAULT '0',
+  `total_ratings` int NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `expert_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `invoice_item`
+--
+
+DROP TABLE IF EXISTS `invoice_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoice_item` (
+  `invoice_item_id` int NOT NULL,
+  `invoice_id` int NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci NOT NULL,
+  `quantity` int NOT NULL DEFAULT '1',
+  `item_total_amount` float NOT NULL,
+  KEY `invoice_item_id` (`invoice_item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `invoices`
+--
+
+DROP TABLE IF EXISTS `invoices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoices` (
+  `invoice_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `invoice_number` varchar(101) COLLATE utf8mb4_general_ci NOT NULL,
+  `issue_date` date NOT NULL,
+  `due_date` date NOT NULL,
+  `total_amount` float NOT NULL DEFAULT '0',
+  `paid_amount` float NOT NULL DEFAULT '0',
+  `status invoice_status` enum('pending','done') COLLATE utf8mb4_general_ci NOT NULL,
+  `notes` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `invoice_number` (`invoice_number`),
+  KEY `invoice_id` (`invoice_id`),
+  CONSTRAINT `invoices_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `invoice_item` (`invoice_item_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -67,7 +240,114 @@ CREATE TABLE `message` (
   KEY `types` (`types`),
   KEY `chatbox_id` (`chatbox_id`),
   CONSTRAINT `message_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `message_ibfk_2` FOREIGN KEY (`chatbox_id`) REFERENCES `chatbox` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `message_ibfk_2` FOREIGN KEY (`chatbox_id`) REFERENCES `chatbox` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `message_ibfk_3` FOREIGN KEY (`id`) REFERENCES `report` (`report_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `notifications`
+--
+
+DROP TABLE IF EXISTS `notifications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notifications` (
+  `notification_id` int NOT NULL AUTO_INCREMENT,
+  `recipient_user_id` int NOT NULL,
+  `notification_type` enum('log in','sign up') COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `message` text COLLATE utf8mb4_general_ci NOT NULL,
+  `is_read` tinyint(1) NOT NULL DEFAULT '0',
+  `read_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `notification_id` (`notification_id`),
+  KEY `recipient_user_id` (`recipient_user_id`),
+  CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`notification_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `payout_requests`
+--
+
+DROP TABLE IF EXISTS `payout_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payout_requests` (
+  `payout_request_id` int NOT NULL AUTO_INCREMENT,
+  `expert_id` int NOT NULL,
+  `requested_amount` float NOT NULL,
+  `status` enum('not yet','sent','done') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'not yet',
+  `requested_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `processed_at` timestamp NOT NULL,
+  `admin_notes` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `payout_request_id` (`payout_request_id`),
+  KEY `expert_id` (`expert_id`),
+  CONSTRAINT `payout_requests_ibfk_1` FOREIGN KEY (`payout_request_id`) REFERENCES `expert_profile` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `report`
+--
+
+DROP TABLE IF EXISTS `report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `report` (
+  `report_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `content` text COLLATE utf8mb4_general_ci NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `report_id` (`report_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `report_ibfk_1` FOREIGN KEY (`report_id`) REFERENCES `calls` (`call_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reviews` (
+  `review_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `expert_id` int NOT NULL,
+  `rating` int NOT NULL,
+  `comment` text COLLATE utf8mb4_general_ci,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `review_id` (`review_id`),
+  KEY `user_id` (`user_id`),
+  KEY `expert_id` (`expert_id`),
+  CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`review_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction` (
+  `transaction_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `invoice_id` int NOT NULL,
+  `amount` float NOT NULL,
+  `status` enum('pending','done') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'pending',
+  `description` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `transaction_id` (`transaction_id`),
+  CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`transaction_id`) REFERENCES `invoices` (`invoice_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -81,7 +361,15 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `user_role` enum('client','expert') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `profile_picture_url` text COLLATE utf8mb4_general_ci,
+  `bio` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone_number` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone_number` (`phone_number`),
+  KEY `create_at` (`create_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
