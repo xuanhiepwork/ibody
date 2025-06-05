6a70b006664c41d326ce3a249f95c63aa6ca5a05
Nguyen Thuan Thanh <thanhntmany@hp1>
2025-06-06 03:27:06 +0700

    add router

--- website/server/http/index.js
+++ website/server/http/index.js
@@ -2,4 +2,8 @@ import express from 'express'
 import { http } from "../server.js"
 import { join } from 'path'
 
+
+// http.use((await import("./router/index.js")).default)
 http.use(express.static(join(import.meta.dirname, "www")))
+// #TODO: http.use(404)
+
---
+++ website/server/http/router/index.js
@@ -0,0 +1,13 @@
+// lib
+import express from 'express'
+
+
+// #TODO:
+const router = express.Router()
+// router.use("/file", (await import('./file/index.js')).default)
+// router.use("/api", (await import('./api/index.js')).default)
+// router.use("/rpc", (await import('./rpc/index.js')).default)
+// router.use("/desk", (await import('./desk/index.js')).default)
+// router.use("/auth", (await import('./auth/index.js')).default)
+
+export default router
