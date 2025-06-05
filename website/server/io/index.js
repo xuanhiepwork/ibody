643bc6eec3c4267fb2a4183e31cdbdced5272e18
Nguyen Thuan Thanh <thanhntmany@hp1>
2025-06-06 04:12:07 +0700

    io

--- website/server/io/index.js
+++ website/server/io/index.js
@@ -2,6 +2,7 @@ import { io } from "../server.js"
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
-
-
