80eee8d2df6e7b8d1ab9169ffa7a7e47c6b06013
Nguyen Thuan Thanh <thanhntmany@hp1>
2025-06-08 03:04:13 +0700

    add cli caller

--- core/app/index.js
+++ core/app/index.js
@@ -14,9 +14,21 @@ const ctx = {
     hasPerm: function (perm) {
         return userHasPerm(this, perm)
     },
-    rpc: async function (modelId, action, ...args) {
-        const model = models[modelId] || (models[modelId] = await import("./model/" + modelId + ".js"))
-        return model[action](this, ...args)
+    rpc: async function (modelId, actionName, ...args) {
+        let model
+        try {
+            model = models[modelId] || (models[modelId] = await import("./model/" + modelId + ".js"))
+        } catch (error) {
+            if (error.code === 'ERR_MODULE_NOT_FOUND') {
+                throw(new Error(`Model ${modelId} not found!`))
+            }
+        }
+
+        if (typeof model[actionName] !== 'function') {
+            throw(new Error(`Action ${modelId}.${actionName} is not a function!`))
+        }
+
+        return model[actionName](this, ...args)
     }
 }
 
--- core/index.js
+++ core/index.js
@@ -1,3 +1,17 @@
-import app from "./app/index.js"
+import ctx from "./app/index.js"
 
-export default app
+if (process.argv.length > 2) {
+    var args = process.argv.slice(2)
+
+    var _ctx = ctx
+    if (args[0] === '-u') {
+        _ctx = _ctx.asUserId(args[1])
+        args = args.slice(2)
+    }
+
+    const modelId = args.shift()
+    const actionName = args.shift()
+    console.log(await _ctx.rpc(modelId, actionName, ...args))
+}
+
+export default ctx
